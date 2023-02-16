8db04a044d6ddb505a03337ce3a3fceb33a6e635
Rares Colf <rares.colf88@gmail.com>
2023-02-16 20:59:49 +0200

    added homepage

--- src/main/java/com/sda/OnlineShop/controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/controller/MainController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -29,11 +33,18 @@ public class MainController {
     }
 
     @PostMapping("/addProduct")
-    public String addProductPost(@ModelAttribute ProductDto productDto) {
-        productService.addProduct(productDto);
+    public String addProductPost(@ModelAttribute ProductDto productDto,
+                                 @RequestParam("productImage") MultipartFile productImage) {
+        productService.addProduct(productDto, productImage);
         System.out.println("S-a apelat functionalitatea de addProductPost");
         System.out.println(productDto);
 
         return "addProduct";
+    }
+@GetMapping("/home")
+    public String homeGet(Model model){
+        List<ProductDto> productDtos = productService.getAllProductDtos();
+        model.addAttribute("productDtos", productDtos);
+        return "home";
     }
 }
