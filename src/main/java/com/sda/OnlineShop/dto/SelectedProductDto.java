e01fda7cc27fcbdc4ef943e342d1acc668d060c3
Catalin Scarlat <catalinscarlat@hotmail.com>
2023-02-23 21:35:13 +0200

    Added ShoppingCart entity and menu

---
+++ src/main/java/com/sda/OnlineShop/dto/SelectedProductDto.java
@@ -0,0 +1,10 @@
+package com.sda.OnlineShop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SelectedProductDto {
+    private String quantity;
+}
--- src/main/java/com/sda/OnlineShop/entities/User.java
+++ src/main/java/com/sda/OnlineShop/entities/User.java
@@ -18,4 +18,6 @@ public class User {
     private String phoneNumber;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+    @OneToOne(mappedBy = "user")
+    private ShoppingCart shoppingCart;
 }
